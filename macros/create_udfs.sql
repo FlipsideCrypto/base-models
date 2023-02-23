24daf1a9da3aff67f0187954654258a4f75769b2
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-02-23 13:06:27 -0500

    Add generic stream

---
+++ macros/create_udfs.sql
@@ -0,0 +1,20 @@
+{% macro create_udfs() %}
+    {% if var("UPDATE_UDFS_AND_SPS") %}
+        {% set sql %}
+        CREATE schema if NOT EXISTS silver;
+        {{ create_udtf_get_base_table(
+            schema = "streamline"
+        ) }}
+
+        {% endset %}
+        {% do run_query(sql) %}
+        {% if target.database != "BASE_COMMUNITY_DEV" %}
+            {% set sql %}
+            {{ create_udf_get_chainhead() }}
+            {{ create_udf_bulk_json_rpc() }}
+
+            {% endset %}
+            {% do run_query(sql) %}
+        {% endif %}
+    {% endif %}
+{% endmacro %}
---
+++ models/silver/streamline/streamline__complete_blocks.sql
@@ -0,0 +1,65 @@
+{{ config (
+    materialized = "incremental",
+    unique_key = "id",
+    cluster_by = "ROUND(block_number, -3)",
+    merge_update_columns = ["id"],
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(id)"
+) }}
+
+WITH meta AS (
+
+    SELECT
+        registered_on,
+        last_modified,
+        file_name
+    FROM
+        TABLE(
+            information_schema.external_table_files(
+                table_name => '{{ source( "bronze_streamline", "blocks") }}'
+            )
+        ) A
+
+{% if is_incremental() %}
+WHERE
+    LEAST(
+        registered_on,
+        last_modified
+    ) >= (
+        SELECT
+            COALESCE(MAX(_INSERTED_TIMESTAMP), '1970-01-01' :: DATE) max_INSERTED_TIMESTAMP
+        FROM
+            {{ this }})
+    ),
+    partitions AS (
+        SELECT
+            DISTINCT TO_DATE(
+                concat_ws('-', SPLIT_PART(file_name, '/', 3), SPLIT_PART(file_name, '/', 4), SPLIT_PART(file_name, '/', 5))
+            ) AS _partition_by_modified_date
+        FROM
+            meta
+    )
+{% else %}
+)
+{% endif %}
+SELECT
+    {{ dbt_utils.surrogate_key(
+        ['block_number']
+    ) }} AS id,
+    block_number,
+    registered_on AS _inserted_timestamp
+FROM
+    {{ source(
+        "bronze_streamline",
+        "blocks"
+    ) }} AS s
+    JOIN meta b
+    ON b.file_name = metadata$filename
+
+{% if is_incremental() %}
+JOIN partitions p
+ON p.partition_block_id = s._partition_by_block_id
+{% endif %}
+
+qualify(ROW_NUMBER() over (PARTITION BY id
+ORDER BY
+    _inserted_timestamp DESC)) = 1
