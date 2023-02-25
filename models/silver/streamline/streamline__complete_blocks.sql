c3f8bc5a9a319fae9a66151b5486d0a6a950acea
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-02-25 10:40:31 -0500

    Update complete blocks and transactions

--- models/silver/streamline/streamline__complete_blocks.sql
+++ models/silver/streamline/streamline__complete_blocks.sql
@@ -6,72 +6,50 @@
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(id)"
 ) }}
 
-WITH max_date AS (
+WITH meta AS (
 
+    SELECT
+        last_modified,
+        file_name
+    FROM
+        TABLE(
+            information_schema.external_table_files(
+                table_name => '{{ source( "bronze_streamline", "blocks") }}'
+            )
+        ) A
+)
+
+{% if is_incremental() %},
+max_date AS (
     SELECT
         COALESCE(MAX(_INSERTED_TIMESTAMP), '1970-01-01' :: DATE) max_INSERTED_TIMESTAMP
     FROM
-        {{ this }}),
-        meta AS (
-            SELECT
-                CAST(
-                    SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER
-                ) AS _partition_by_block_number
-            FROM
-                TABLE(
-                    information_schema.external_table_files(
-                        table_name => '{{ source( "bronze_streamline", "blocks") }}'
-                    )
-                ) A
+        {{ this }})
+    {% endif %}
+    SELECT
+        {{ dbt_utils.surrogate_key(
+            ['block_number']
+        ) }} AS id,
+        block_number,
+        last_modified AS _inserted_timestamp
+    FROM
+        {{ source(
+            "bronze_streamline",
+            "blocks"
+        ) }}
+        JOIN meta b
+        ON b.file_name = metadata$filename
 
 {% if is_incremental() %}
 WHERE
-    last_modified >= (
+    b.last_modified > (
         SELECT
-            MAX(max_INSERTED_TIMESTAMP)
+            max_INSERTED_TIMESTAMP
         FROM
             max_date
     )
 {% endif %}
-)
-SELECT
-    MD5(
-        CAST(COALESCE(CAST(block_number AS text), '') AS text)
-    ) AS id,
-    block_number,
-    (
 
-{% if is_incremental() %}
-SELECT
-    MAX(max_INSERTED_TIMESTAMP)
-FROM
-    max_date
-{% else %}
-    SYSDATE()
-{% endif %}) AS _inserted_timestamp
-FROM
-    {{ source(
-        "bronze_streamline",
-        "blocks"
-    ) }}
-    t
-    JOIN meta b
-    ON b._partition_by_block_number = t._partition_by_block_id
-WHERE
-    b._partition_by_block_number = t._partition_by_block_id
-    AND DATA :error :code IS NULL
-    OR DATA :error :code NOT IN (
-        '-32000',
-        '-32001',
-        '-32002',
-        '-32003',
-        '-32004',
-        '-32005',
-        '-32006',
-        '-32007',
-        '-32008',
-        '-32009',
-        '-32010'
-    ) qualify(ROW_NUMBER() over (PARTITION BY id
+qualify(ROW_NUMBER() over (PARTITION BY id
 ORDER BY
     _inserted_timestamp DESC)) = 1
