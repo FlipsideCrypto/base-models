1b96f856458f405dc64a43b9eae7468415ac2934
austin <austin@flipsidecrypto.com>
2023-02-27 12:45:22 -0500

    gold models

---
+++ models/gold/testnet/testnet__fact_blocks.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized = 'view',
+    persist_docs ={ "relation": true,
+    "columns": true }
+) }}
+
+SELECT
+    A.block_number AS block_number,
+    block_timestamp,
+    network,
+    blockchain,
+    tx_count,
+    difficulty,
+    total_difficulty,
+    extra_data,
+    gas_limit,
+    gas_used,
+    HASH,
+    parent_hash,
+    receipts_root,
+    sha3_uncles,
+    SIZE,
+    uncle_blocks,
+    block_header_json
+FROM
+    {{ ref('silver__blocks') }} A
+
