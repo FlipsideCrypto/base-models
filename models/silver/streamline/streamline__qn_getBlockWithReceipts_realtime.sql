d9f0836e567204c041c21817a461dc05d8b4ecac
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-02-28 11:09:45 -0500

    Add partitioning to complete models, update realtime json parsing (#9)

--- models/silver/streamline/streamline__qn_getBlockWithReceipts_realtime.sql
+++ models/silver/streamline/streamline__qn_getBlockWithReceipts_realtime.sql
@@ -6,6 +6,22 @@
     )
 ) }}
 
+WITH blocks AS (
+
+    SELECT
+        block_number :: STRING AS block_number
+    FROM
+        {{ ref("streamline__blocks") }}
+    WHERE
+        block_number > 1000000
+    EXCEPT
+    SELECT
+        block_number :: STRING
+    FROM
+        {{ ref("streamline__complete_qn_getBlockWithReceipts") }}
+    WHERE
+        block_number > 1000000
+)
 SELECT
     PARSE_JSON(
         CONCAT(
@@ -18,14 +34,4 @@ SELECT
         )
     ) AS request
 FROM
-    {{ ref("streamline__blocks") }}
-WHERE
-    block_number > 1000000
-    AND block_number IS NOT NULL
-EXCEPT
-SELECT
-    block_number
-FROM
-    {{ ref("streamline__complete_qn_getBlockWithReceipts") }}
-WHERE
-    block_number > 1000000
+    blocks
