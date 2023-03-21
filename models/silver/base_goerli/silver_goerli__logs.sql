b73443fc1c3dbfc57ac2e686eaf9732572b18a77
austin <austin@flipsidecrypto.com>
2023-03-21 17:54:51 -0400

    join key

--- models/silver/base_goerli/silver_goerli__logs.sql
+++ models/silver/base_goerli/silver_goerli__logs.sql
@@ -6,82 +6,84 @@
 
 WITH flat_base AS (
 
-SELECT
-    _log_id,
-    block_number,
-    blockHash AS block_hash,
-    tx_hash,
-    origin_from_address,
-    CASE
-        WHEN len(origin_to_address) <= 0 THEN NULL
-        ELSE origin_to_address
-    END AS origin_to_address,
-    event_index,
-    contract_address,
-    topics,
-    DATA,
-    CASE
-        WHEN removed = 'true' THEN TRUE
-        ELSE FALSE
-    END AS event_removed,
-    CASE
-        WHEN status = '0x1' THEN 'SUCCESS'
-        ELSE 'FAIL'
-    END AS tx_status,
-    ethereum.public.udf_hex_to_int(
-        transactionIndex
-    ) :: INTEGER AS tx_index,
-    ethereum.public.udf_hex_to_int(
-        TYPE
-    ) :: INTEGER AS TYPE,
-    _inserted_timestamp
-FROM
-    {{ ref('silver_goerli__receipts_method') }}
-WHERE
-    tx_hash IS NOT NULL
+    SELECT
+        _log_id,
+        block_number,
+        blockHash AS block_hash,
+        tx_hash,
+        origin_from_address,
+        CASE
+            WHEN len(origin_to_address) <= 0 THEN NULL
+            ELSE origin_to_address
+        END AS origin_to_address,
+        event_index,
+        contract_address,
+        topics,
+        DATA,
+        CASE
+            WHEN removed = 'true' THEN TRUE
+            ELSE FALSE
+        END AS event_removed,
+        CASE
+            WHEN status = '0x1' THEN 'SUCCESS'
+            ELSE 'FAIL'
+        END AS tx_status,
+        ethereum.public.udf_hex_to_int(
+            transactionIndex
+        ) :: INTEGER AS tx_index,
+        ethereum.public.udf_hex_to_int(
+            TYPE
+        ) :: INTEGER AS TYPE,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver_goerli__receipts_method') }}
+    WHERE
+        tx_hash IS NOT NULL
 
 {% if is_incremental() %}
-and
-    _inserted_timestamp >= (
-        SELECT
-            MAX(
-                _inserted_timestamp
-            )
-        FROM
-            {{ this }}
-    )
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        )
+    FROM
+        {{ this }}
+)
 {% endif %}
 ),
-
 new_records AS (
-SELECT
-    f.block_number,
-    b.block_timestamp,
-    f.block_hash,
-    f.tx_hash,
-    f.origin_from_address,
-    f.origin_to_address,
-    f.event_index,
-    f.contract_address,
-    f.topics,
-    f.DATA,
-    f.event_removed,
-    f.tx_status,
-    f.tx_index,
-    f.TYPE,
-    t.origin_function_signature,
-    CASE
-        WHEN t.origin_function_signature IS NULL
+    SELECT
+        f.block_number,
+        b.block_timestamp,
+        f.block_hash,
+        f.tx_hash,
+        f.origin_from_address,
+        f.origin_to_address,
+        f.event_index,
+        f.contract_address,
+        f.topics,
+        f.data,
+        f.event_removed,
+        f.tx_status,
+        f.tx_index,
+        f.type,
+        t.origin_function_signature,
+        CASE
+            WHEN t.origin_function_signature IS NULL
             OR b.block_timestamp IS NULL THEN TRUE
-        ELSE FALSE
-    END AS is_pending,
-    f._log_id,
-    f._inserted_timestamp
-FROM flat_base f 
-LEFT OUTER JOIN {{ ref('silver_goerli__transactions') }} t 
-    ON f.tx_hash = t.tx_hash AND f.block_number = t.block_number
-LEFT OUTER JOIN {{ ref('silver_goerli__blocks') }} b
-    ON f.block_number = t.block_number
+            ELSE FALSE
+        END AS is_pending,
+        f._log_id,
+        f._inserted_timestamp
+    FROM
+        flat_base f
+        LEFT OUTER JOIN {{ ref('silver_goerli__transactions') }}
+        t
+        ON f.tx_hash = t.tx_hash
+        AND f.block_number = t.block_number
+        LEFT OUTER JOIN {{ ref('silver_goerli__blocks') }}
+        b
+        ON f.block_number = b.block_number
 )
 
 {% if is_incremental() %},
@@ -96,11 +98,11 @@ missing_data AS (
         t.event_index,
         t.contract_address,
         t.topics,
-        t.DATA,
+        t.data,
         t.event_removed,
         t.tx_status,
         t.tx_index,
-        t.TYPE,
+        t.type,
         txs.origin_function_signature,
         FALSE AS is_pending,
         t._log_id,
@@ -109,17 +111,21 @@ missing_data AS (
             b._inserted_timestamp,
             txs._inserted_timestamp
         ) AS _inserted_timestamp
-    FROM {{ this }} t
-    INNER JOIN {{ ref('silver_goerli__transactions') }} txs
-        ON t.tx_hash = txs.tx_hash AND t.block_number = txs.block_number
-    INNER JOIN {{ ref('silver_goerli__blocks') }} b
+    FROM
+        {{ this }}
+        t
+        INNER JOIN {{ ref('silver_goerli__transactions') }}
+        txs
+        ON t.tx_hash = txs.tx_hash
+        AND t.block_number = txs.block_number
+        INNER JOIN {{ ref('silver_goerli__blocks') }}
+        b
         ON t.block_number = b.block_number
     WHERE
         t.is_pending
 )
 {% endif %}
-
-SELECT 
+SELECT
     block_number,
     block_timestamp,
     block_hash,
@@ -138,9 +144,10 @@ SELECT
     is_pending,
     _log_id,
     _inserted_timestamp
-FROM new_records
-QUALIFY(ROW_NUMBER() OVER (PARTITION BY block_number, _log_id 
-    ORDER BY _inserted_timestamp DESC)) = 1
+FROM
+    new_records qualify(ROW_NUMBER() over (PARTITION BY block_number, _log_id
+ORDER BY
+    _inserted_timestamp DESC)) = 1
 
 {% if is_incremental() %}
 UNION
