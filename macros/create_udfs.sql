f98d241b15a37a245d22de612a5ff00da0f31130
drethereum <trevor.wenokur@gmail.com>
2023-06-06 11:41:59 -0600

    create udfs

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -17,9 +17,6 @@
             {% endset %}
             {% do run_query(sql) %}
         {% endif %}
-        {% set name %}
         {{- fsc_utils.create_udfs() -}}
-        {% endset %}
-        {% do run_query(sql) %}
     {% endif %}
 {% endmacro %}
