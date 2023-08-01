dfb8f5b81848256bdd1646bb89641d6a61712de7
drethereum <71602799+drethereum@users.noreply.github.com>
2023-08-01 11:47:55 -0600

    reorg/folder-tests (#45)
    * folder structure and tags
    
    * python version and caching
    
    * observability tag on test
    
    * -m
    
    * renamed job to chainhead
    
    * removed legacy workflows
    
    * decoder workflows

---
+++ models/legacy_models/silver_goerli/eth_goerli/silver__eth_goerli_contract_reads.sql
@@ -0,0 +1,26 @@
+{{ config (
+    materialized = "table",
+    unique_key = "batch_id",
+    tags = ['base_goerli']
+) }}
+
+WITH ranges AS (
+
+    SELECT
+        batch_id AS group_id,
+        fromBlock,
+        toBlock,
+        contract_address
+    FROM
+        {{ ref('silver__eth_goerli_block_ranges') }}
+        JOIN {{ ref('silver__eth_goerli_contracts') }}
+        ON 1 = 1
+)
+SELECT
+    ROW_NUMBER() over (
+        ORDER BY
+            group_id,
+            contract_address
+    ) AS batch_id,*
+FROM
+    ranges
