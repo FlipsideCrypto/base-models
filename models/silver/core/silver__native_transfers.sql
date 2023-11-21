e7d2b8ed06d6620c2a34e5a15820c5b9fa74ad58
Austin <austin@flipsidecrypto.com>
2023-11-21 09:03:13 -0500

    SO

--- models/silver/core/silver__native_transfers.sql
+++ models/silver/core/silver__native_transfers.sql
@@ -3,6 +3,7 @@
     incremental_strategy = 'delete+insert',
     unique_key = 'block_number',
     cluster_by = ['block_timestamp::DATE'],
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
     tags = ['core','non_realtime','reorg']
 ) }}
 
