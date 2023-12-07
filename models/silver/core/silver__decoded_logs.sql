2e5a75c3933750a9e35026955762340cba53f544
drethereum <trevor.wenokur@gmail.com>
2023-12-07 14:38:52 -0700

    hot swap

--- models/silver/core/silver__decoded_logs.sql
+++ models/silver/core/silver__decoded_logs.sql
@@ -3,9 +3,10 @@
     materialized = "incremental",
     unique_key = ['block_number', 'event_index'],
     cluster_by = "block_timestamp::date",
+    incremental_predicates = ["dynamic_range", "block_number"],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
-    full_refresh = false,
     merge_exclude_columns = ["inserted_timestamp"],
+    full_refresh = false,
     tags = ['decoded_logs','reorg']
 ) }}
 
@@ -39,15 +40,16 @@ WHERE
         FROM
             {{ this }}
     )
+    AND DATA NOT ILIKE '%Event topic is not present in given ABI%'
 {% else %}
     {{ ref('bronze__fr_decoded_logs') }}
 WHERE
-    block_number <= 10000000
+    DATA NOT ILIKE '%Event topic is not present in given ABI%'
 {% endif %}
 
 qualify(ROW_NUMBER() over (PARTITION BY block_number, event_index
 ORDER BY
-    _inserted_timestamp DESC)) = 1
+    _inserted_timestamp DESC, _partition_by_created_date DESC)) = 1
 ),
 transformed_logs AS (
     SELECT
@@ -59,7 +61,7 @@ transformed_logs AS (
         decoded_data,
         _inserted_timestamp,
         _log_id,
-        ethereum.silver.udf_transform_logs(decoded_data) AS transformed
+        utils.udf_transform_logs(decoded_data) AS transformed
     FROM
         base_data
 ),
