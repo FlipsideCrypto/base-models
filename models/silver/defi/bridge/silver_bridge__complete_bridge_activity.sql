733a2b2e0c65270de53e5b9b5d84fc4b29b21114
drethereum <trevor.wenokur@gmail.com>
2023-12-22 09:53:13 -0700

    remove multichain

--- models/silver/defi/bridge/silver_bridge__complete_bridge_activity.sql
+++ models/silver/defi/bridge/silver_bridge__complete_bridge_activity.sql
@@ -1,4 +1,4 @@
-{# {{ config(
+{{ config(
     materialized = 'incremental',
     incremental_strategy = 'delete+insert',
     unique_key = ['block_number','platform','version'],
@@ -171,41 +171,6 @@ meson AS (
         _inserted_timestamp
     FROM
         {{ ref('silver_bridge__meson_transfers') }}
-
-{% if is_incremental() %}
-WHERE
-    _inserted_timestamp >= (
-        SELECT
-            MAX(_inserted_timestamp) - INTERVAL '36 hours'
-        FROM
-            {{ this }}
-    )
-{% endif %}
-),
-multichain AS (
-    SELECT
-        block_number,
-        block_timestamp,
-        origin_from_address,
-        origin_to_address,
-        origin_function_signature,
-        tx_hash,
-        event_index,
-        bridge_address,
-        event_name,
-        platform,
-        'v1' AS version,
-        sender,
-        receiver,
-        destination_chain_receiver,
-        destination_chain_id :: STRING AS destination_chain_id,
-        NULL AS destination_chain,
-        token_address,
-        amount AS amount_unadj,
-        _log_id AS _id,
-        _inserted_timestamp
-    FROM
-        {{ ref('silver_bridge__multichain_v7_loganyswapout') }}
 
 {% if is_incremental() %}
 WHERE
@@ -421,11 +386,6 @@ all_protocols AS (
     SELECT
         *
     FROM
-        multichain
-    UNION ALL
-    SELECT
-        *
-    FROM
         stargate
     UNION ALL
     SELECT
@@ -561,4 +521,4 @@ SELECT
 FROM
     FINAL qualify (ROW_NUMBER() over (PARTITION BY _id
 ORDER BY
-    _inserted_timestamp DESC)) = 1 #}
+    _inserted_timestamp DESC)) = 1
