8504042385a072ef75d893b52dd0cce78c43e832
mattromano <mattromano88@gmail.com>
2024-09-24 19:53:30 -0700

    add aerodrome-slipstream models

---
+++ models/silver/defi/dex/aerodrome/silver_dex__aerodrome_slipstream_pools.sql
@@ -0,0 +1,99 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "pool_address",
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['curated']
+) }}
+
+WITH created_pools AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        contract_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        LOWER(CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40))) AS token0_address,
+        LOWER(CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40))) AS token1_address,
+        utils.udf_hex_to_int(
+            's2c',
+            topics [3] :: STRING
+        ) :: INTEGER AS tick_spacing,
+        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS pool_address,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+    WHERE
+        topics [0] = '0xab0d57f0df537bb25e80245ef7748fa62353808c54d6e528a9dd20887aed9ac2'
+        AND contract_address = lower('0x5e7BB104d84c7CB9B682AaC2F3d509f5F406809A')
+        AND tx_status = 'SUCCESS'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        ) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+initial_info AS (
+    SELECT
+        contract_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        utils.udf_hex_to_int('s2c', CONCAT('0x', segmented_data [0] :: STRING)) :: FLOAT AS init_sqrtPriceX96,
+        utils.udf_hex_to_int('s2c', CONCAT('0x', segmented_data [1] :: STRING)) :: FLOAT AS init_tick,
+        pow(
+            1.0001,
+            init_tick
+        ) AS init_price_1_0_unadj,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+    WHERE
+        topics [0] :: STRING = '0x98636036cb66a9c19a37435efc1e90142190214e8abeb821bdba3f2990dd4c95'
+        AND tx_status = 'SUCCESS'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        ) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+FINAL AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        p.contract_address,
+        token0_address,
+        token1_address,
+        tick_spacing,
+        pool_address,
+        COALESCE(
+            init_tick,
+            0
+        ) AS init_tick,
+        p._log_id AS _id,
+        p._inserted_timestamp
+    FROM
+        created_pools p
+        LEFT JOIN initial_info i
+        ON p.pool_address = i.contract_address
+)
+SELECT
+    *
+FROM
+    FINAL qualify(ROW_NUMBER() over(PARTITION BY pool_address
+ORDER BY
+    _inserted_timestamp DESC)) = 1
