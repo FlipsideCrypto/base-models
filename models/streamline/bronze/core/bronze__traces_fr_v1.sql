ea798470669aed1fed9549b30e71253ddded208a
drethereum <trevor.wenokur@gmail.com>
2024-12-06 18:49:32 +1200

    dbt project vars, core bronze views and fsc-evm temp macros

--- models/streamline/bronze/core/bronze__traces_fr_v1.sql
+++ models/streamline/bronze/core/bronze__traces_fr_v1.sql
@@ -1,5 +1,5 @@
 {# Set variables #}
-{% set source_name = 'TRACES' %}
+{% set source_name = 'DEBUG_TRACEBLOCKBYNUMBER' if var('GLOBAL_USES_SINGLE_FLIGHT_METHOD',false) else 'TRACES' %}
 {% set source_version = '' %}
 {% set model_type = 'FR' %}
 
