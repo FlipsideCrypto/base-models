20de9f0014ca858a516f24e60711e4006d0fcc54
drethereum <trevor.wenokur@gmail.com>
2025-04-08 16:25:47 -0600

    false

--- models/silver/overflow/silver__overflowed_traces.sql
+++ models/silver/overflow/silver__overflowed_traces.sql
@@ -6,6 +6,7 @@
     unique_key = ['block_number','tx_position'],
     cluster_by = ['modified_timestamp::DATE','partition_key'],
     tags = ['overflowed_traces'],
+    full_refresh = false,
     snowflake_warehouse = warehouse
 ) }}
 
